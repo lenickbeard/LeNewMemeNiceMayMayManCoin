19bd0c9fa396b4dae82f4c7c5e9200e5b626290f
claudeclaire <inteltwitch@gmail.com>
2014-03-15 12:00:00 +0100

    Initial release of LeNewMemeNiceMayMayManCoin v0.1-alpha

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// LeNewMemeNiceMayMayManCoin Core - v0.1
+// (c) 2014 May May Enterprises
+#include <iostream>
+
+int main() {
+    std::cout << "Starting Nice May May Man Node..." << std::endl;
+    std::cout << "Loading ironic consensus protocol..." << std::endl;
+    std::cout << "Meme status: VERY NICE" << std::endl;
+    return 0;
+}
